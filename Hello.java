ac87c3d27b6c225561fff1153e72954adc04f852
gibsona451 <gibsona451@strose.edu>
2016-09-29 09:40:19 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 public class Hello
 {
 	public static void main(String[] args)
-	{
+	{	// sup dude <3 
 		//This line prints my name
 		System.out.println("Kate Pierce");
+		System.out.println ("Alex was here!");
 	}
 }
